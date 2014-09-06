6a8797d5798dff1b4111e3644086f0719f617b91
Skylar Saveland <skylar.saveland@gmail.com>
2014-09-05 18:36:16 -0700

    fixes #21 - update_songs.py
    update songs.json manually, for now.
    We can hook into some process later

---
+++ update_songs.py
@@ -0,0 +1,24 @@
+import os
+import json
+import yaml
+
+
+songs = []
+
+for melt in os.listdir("melts/"):
+    if os.path.splitext(melt)[-1] != ".melt":
+        continue
+    fn = "melts/%s" % melt
+    with open(fn) as f:
+        print(fn)
+        txt = f.read()
+        meta = txt.split("===")[1]
+        meta = yaml.load(meta)
+        meta["file"] = fn
+        songs.append(meta)
+
+with open("songs.json", "w") as f:
+    f.write(json.dumps(songs, indent=4))
+
+import IPython; IPython.embed()
+
